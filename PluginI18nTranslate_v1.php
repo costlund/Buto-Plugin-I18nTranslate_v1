9ff3d72e28ebc1808403256acbbd68a5c0f75f0c
Claes Östlund <claesostlund@Mefromhd-MBP18.local>
2018-11-26 14:55:13 +0100

    Improved log.

--- PluginI18nTranslate_v1.php
+++ PluginI18nTranslate_v1.php
@@ -1,28 +1,18 @@
 <?php
-/**
-<p>Event to translate innerHTML param for an element.</p>
-<p>Example to translate the word About to Finnish.</p>
-<ul>
-<li>Create folder /theme/_parent_/_child_/i18n.
-<li>Create file /theme/_parent_/_child_/i18n/fi.yml.
-<li>Register translations in the file fi.yml.
-<li>Set the event listener and the i18n param in theme settings.yml.
-</ul>
-<p>This settings in theme settings.yml along with event settings.</p>
-#code-yml#
-i18n:
-  language: fi
-#code#
-<p>This in fi.yml</p>
-#code-yml#
-About: Noin
-#code#
- */
 class PluginI18nTranslate_v1{
   /**
    <p>Set in param events/document_render_string in theme settings.yml.</p>
    */
   public static function event_translate_string($value, $string){
+    if(is_numeric($string)){
+      return $string;
+    }
+    if(strstr($string, '<')){
+      return $string;
+    }
+    if(strstr($string, 'item[{')){
+      return $string;
+    }
     $i18n = new PluginI18nTranslate_v1();
     $string = $i18n->translateFromTheme($string);
     return $string;
@@ -34,6 +24,21 @@ class PluginI18nTranslate_v1{
     $data = $this->getData($innerHTML);
     if($data && isset($data[$innerHTML])){
       $innerHTML = $data[$innerHTML];
+    }elseif($data){
+      /**
+       * Log.
+       */
+      if(wfConfig::get('plugin/i18n/translate_v1/settings/log')){
+        $log = true;
+        if(wfConfig::get('plugin/i18n/translate_v1/settings/log_domain_filter') && !strstr(wfServer::getServerName(), wfConfig::get('plugin/i18n/translate_v1/settings/log_domain_filter'))){
+          $log = false;
+        }
+        if($log){
+          $language = wfI18n::getLanguage();
+          $path = $this->getPath();
+          $this->log($path, $language, $innerHTML);
+        }
+      }
     }
     /**
      * Replace.
@@ -45,19 +50,16 @@ class PluginI18nTranslate_v1{
     }
     return $innerHTML;
   }
-  public function getData($innerHTML){
+  /**
+   * Get all translations for one language.
+   * @return array
+   */
+  public function getData($innerHTML = null){
     $data = null;
     /**
      * Path to translations files.
      */
-    $path = '/theme/[theme]/i18n';
-    /**
-     * Check if path is changed via theme settings file.
-     */
-    $settings = wfPlugin::getPluginSettings('i18n/translate_v1', true);
-    if($settings && $settings->get('settings/path')){
-      $path = $settings->get('settings/path');
-    }
+    $path = $this->getPath();
     /**
      * Retreive language.
      */
@@ -70,30 +72,35 @@ class PluginI18nTranslate_v1{
       if(wfFilesystem::fileExist(wfArray::get($GLOBALS, 'sys/app_dir').$filename)){
         $data = wfSettings::getSettings($filename);
       }
-      if(wfConfig::get('plugin/i18n/translate_v1/settings/log')){
-        if(is_null($data)){
-          $this->log($path, $language, 'file_missing', $innerHTML);
-        }elseif(!isset($data[$innerHTML])){
-          $this->log($path, $language, 'key_missing', $innerHTML);
-        }
-      }
     }
     return $data;
   }
+  private function getPath(){
+    /**
+     * Path to translations files.
+     */
+    $path = '/theme/[theme]/i18n';
+    /**
+     * Check if path is changed via theme settings file.
+     */
+    $settings = wfPlugin::getPluginSettings('i18n/translate_v1', true);
+    if($settings && $settings->get('settings/path')){
+      $path = $settings->get('settings/path');
+    }
+    return $path;
+  }
   /**
-   * Log issues.
-   * @param type $path
-   * @param type $language
-   * @param type $type
-   * @param type $innerHTML
-   * @return type
+   * Log when no match.
+   * @param string $path
+   * @param string $language
+   * @param string $innerHTML
+   * @return null
    */
-  private function log($path, $language, $type, $innerHTML){
+  private function log($path, $language, $innerHTML){
     wfPlugin::includeonce('wf/yml');
-    $filename = 'log_'.date('ymd').'.yml';
+    $filename = $language.'_log.yml';
     $logfile = new PluginWfYml(wfGlobals::getAppDir().$path.'/'.$filename);
-    $array = array('la' => $language, 'time' => date('Y-m-d H:i:s'), 'type' => $type, 'innerHTML' => $innerHTML);
-    $logfile->set(wfCrypt::getUid(), $array);
+    $logfile->set($innerHTML.'/', date('ymd H:i:s'));
     $logfile->save();
     return null;
   }
